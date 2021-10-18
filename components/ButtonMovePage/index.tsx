8c4f95ca32bdeac202bad1a8b5ee35e93ab3a9e0
Jônatas Souza Soares <56240097+jonatasJS@users.noreply.github.com>
2021-10-18 00:57:29 -0300

    '-

--- components/ButtonMovePage/index.tsx
+++ components/ButtonMovePage/index.tsx
@@ -5,6 +5,7 @@ import styles from '../../styles/Home.module.css';
 
 export default function ButtonMovePage() {
   const [isActive, setIsActive] = useState('none');
+
   useEffect(function mont() {
     function onScroll() {
       if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -15,10 +16,6 @@ export default function ButtonMovePage() {
     }
 
     window.addEventListener("scroll", onScroll);
-
-    return function unMount() {
-      window.addEventListener("scroll", onScroll);
-    }
   });
 
   function toTop() {
