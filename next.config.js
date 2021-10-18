203eaf1ec4713f5211a4684ece10c31748c92b51
Jônatas Souza Soares <56240097+jonatasJS@users.noreply.github.com>
2021-10-18 00:22:15 -0300

    Resolve Error

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-<<<<<<< HEAD
     domains: [ 'lh3.googleusercontent.com', 'lipis.github.io' ]
-=======
-    domains: [ 'lh3.googleusercontent.com', '*.google.com' ]
->>>>>>> 8ce70a3744da47769f8ecfd86e2b25e61a5c4e10
   }
 }
