188004f0cb64d5a79516ded09851d499edf51916
Jônatas Souza Soares <56240097+jonatasJS@users.noreply.github.com>
2021-10-24 10:42:09 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: [ 'lh3.googleusercontent.com', 'lipis.github.io' ]
+    domains: [ 'lh3.googleusercontent.com', 'lipis.github.io', 'lipis.dev' ],
+    minimumCacheTTL: 60,
   }
 }
