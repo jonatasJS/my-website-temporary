1bbf61d3901391f38091e02cf501b4cee9bbb572
jonatasJS <soaresjonatas398@gmail.com>
2021-12-25 21:40:16 -0300

    fix

---
+++ out/_next/static/ttkcXeSIXXhITnCiwqvnn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ea32797c5e4f00bfdde9.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
