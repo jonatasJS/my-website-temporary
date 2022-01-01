29df524693d67e7762445a375184c6cb2d0376fb
sozinho <soaresjonatas398@gmail.com>
2022-01-01 13:15:05 +0000

    TESTE

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
 import Script from 'next/script';
+import Router from 'next/router';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '../contexts/ThemeContext';
 import { motion } from 'framer-motion';
+import NProgress from 'nprogress';
 
 import '../styles/globals.css';
 import '../styles/global.ts';
@@ -14,13 +16,20 @@ import ButtonMovePage from "../components/ButtonMovePage";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
+Router.events.on("routeChangeStart", () => NProgress.start())
+Router.events.on("routeChangeComplete", () => NProgress.done())
+Router.events.on("routeChangeError", () => NProgress.done())
+
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <ThemeProvider>
       <Head>
+        <link rel="stylesheet" href="nprogress.css" />
         <link rel="icon" href={"/next-rocket-logo.png" || "https://i.imgur.com/XG63jgi.png"} />
       </Head>
-      <motion.div
+      <div>
+        {/* <ChangeLanguage /> */}
+        <motion.div
         key={router.route}
         initial="pageInitial"
         animate='pageAnimate'
@@ -36,7 +45,12 @@ function MyApp({ Component, pageProps, router }: AppProps) {
         opacity: 0
       }}
       >
-        <Script
+        <Navbar />
+        <ButtonMovePage />
+        <Component {...pageProps} />
+      </motion.div>
+      <Footer />
+      <Script
           async
           strategy="lazyOnload"
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
@@ -60,14 +74,7 @@ function MyApp({ Component, pageProps, router }: AppProps) {
           });
         `}
         </Script>
-        <Navbar />
-        {/* <ChangeLanguage /> */}
-        <ButtonMovePage />
-
-        <Component {...pageProps} />
-
-        <Footer />
-      </motion.div>
+        </div>
     </ThemeProvider>
   )
 }
